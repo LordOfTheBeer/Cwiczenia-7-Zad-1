2b069ba5f087429eccac40c8329827e0be6b9d35
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 16:25:58 +0100

    Add files via upload

---
+++ Cwiczenia 7 Zad 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Cwiczenia_7_Zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sH;
+
+            sH = 2 / (1 / 200.0 + 1 / 300.0);
+            
+            Console.WriteLine(sH);
+
+        }
+    }
+}
